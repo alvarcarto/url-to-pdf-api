7eca0341a136b75907c58287a8d16f625be85e31
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-09-29 11:42:07 +0300

    Implement parameters for GET request

--- src/http/pdf-http.js
+++ src/http/pdf-http.js
@@ -3,10 +3,43 @@ const pdfCore = require('../core/pdf-core');
 
 const getRender = ex.createRoute((req, res) => {
   const opts = {
-    // TODO: add all params here
+    url: req.query.url,
+    scrollPage: req.query.scrollPage,
+    emulateScreenMedia: req.query.emulateScreenMedia,
+    waitFor: req.query.waitFor,
+    viewport: {
+      width: req.query['viewport.width'],
+      height: req.query['viewport.height'],
+      deviceScaleFactor: req.query['viewport.deviceScaleFactor'],
+      isMobile: req.query['viewport.isMobile'],
+      hasTouch: req.query['viewport.hasTouch'],
+      isLandscape: req.query['viewport.isLandscape'],
+    },
+    goto: {
+      timeout: req.query['goto.timeout'],
+      waitUntil: req.query['goto.waitUntil'],
+      networkIdleInflight: req.query['goto.networkIdleInflight'],
+      networkIdleTimeout: req.query['goto.networkIdleTimeout'],
+    },
+    pdf: {
+      scale: req.query['pdf.scale'],
+      displayHeaderFooter: req.query['pdf.displayHeaderFooter'],
+      landscape: req.query['pdf.landscape'],
+      pageRanges: req.query['pdf.pageRanges'],
+      format: req.query['pdf.format'],
+      width: req.query['pdf.width'],
+      height: req.query['pdf.height'],
+      margin: {
+        top: req.query['pdf.margin.top'],
+        right: req.query['pdf.margin.right'],
+        bottom: req.query['pdf.margin.bottom'],
+        left: req.query['pdf.margin.left'],
+      },
+      printBackground: req.query['pdf.printBackground'],
+    },
   };
 
-  return pdfCore.render(req.query)
+  return pdfCore.render(opts)
     .then((data) => {
       res.set('content-type', 'application/pdf');
       res.send(data);
