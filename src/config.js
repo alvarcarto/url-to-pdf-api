92c7d78723c78c596e6914968f316fd166cea881
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2019-05-14 14:39:22 +0300

    Add CORS_ORIGIN to config.js

--- src/config.js
+++ src/config.js
@@ -7,6 +7,7 @@ const config = {
   LOG_LEVEL: process.env.LOG_LEVEL,
   ALLOW_HTTP: process.env.ALLOW_HTTP === 'true',
   DEBUG_MODE: process.env.DEBUG_MODE === 'true',
+  CORS_ORIGIN: process.env.CORS_ORIGIN || '*',
   API_TOKENS: [],
 };
 
